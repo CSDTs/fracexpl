09ca2186d6fbd91d07d23750c7a53b3283b1bd27
holmr <holmr@rpi.edu>
2018-02-27 13:16:31 -0500

    using style instead of classname

--- src/fracexpl.js
+++ src/fracexpl.js
@@ -786,10 +786,10 @@ SeedEditor.prototype.setSegType = function(type) {
   document.removeEventListener ("mouseup" , this.onMouseUp , false);
   if (type != this.currentSegType) {
     if (this.currentSegType != -1) {
-      this.segTypeBtn[this.currentSegType].classList.remove("highlighted");
+      this.segTypeBtn[this.currentSegType].style.border = '';
       this.segTypeBtn[this.currentSegType].disabled = false;
     }
-    this.segTypeBtn[type].classList.add("highlighted");
+    this.segTypeBtn[type].style.border = '2px solid rgb(236, 65, 168)';
     this.segTypeBtn[type].disabled = true;
     this.currentSegType = type;
 
