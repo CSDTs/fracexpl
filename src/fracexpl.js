5d3af98eaa5ceb0b1eb4a93541367a526bc7e4c7
holmr <holmr@rpi.edu>
2018-04-25 09:52:09 -0400

    removing comments

--- src/fracexpl.js
+++ src/fracexpl.js
@@ -170,32 +170,20 @@ FractalDraw.prototype.scaleSeed = function(seed, scaleFactor) {
 FractalDraw.prototype.highResScreenShot = function(canvas) {
   var lnk = document.createElement('a'),
       e;
-
-  /// the key here is to set the download attribute of the a tag
   lnk.download = 'download';
-
-  /// convert canvas content to data-uri for link. When download
-  /// attribute is set the content pointed to by link will be
-  /// pushed as "download" in HTML5 capable browsers
   let ctx = canvas.getContext('2d');
   ctx.mozImageSmoothingEnabled = false;
   ctx.imageSmoothingEnabled = false;
   ctx.webkitImageSmoothingEnabled = false;
   ctx.scale(4,4);
   lnk.href = canvas.toDataURL();
-
-  /// create a "fake" click-event to trigger the download
   if (document.createEvent) {
-
       e = document.createEvent("MouseEvents");
       e.initMouseEvent("click", true, true, window,
                         0, 0, 0, 0, 0, false, false, false,
                         false, 0, null);
-
       lnk.dispatchEvent(e);
-
   } else if (lnk.fireEvent) {
-
       lnk.fireEvent("onclick");
   }
 };
